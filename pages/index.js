230a068d7822ade872e3c7e5605613b4ed9a483f
xavi <javier19pedraza@gmail.com>
2023-11-09 14:33:15 -0500

    aorund

--- pages/index.js
+++ pages/index.js
@@ -17,7 +17,7 @@ export default function Home() {
 
   return (
     <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24`}
+      className={`flex min-h-screen flex-col items-center justify-around p-24`}
     >
       <h1 className={`text-4xl font-bold`}>Hello Counter</h1>
 
